d9d2f3474308dd74f5a91194501137378b7cfcbc
Pedro Queiroz <pedro.queiroz@thoughtworks.com>
2019-07-19 11:41:20 -0300

    Use global configuration for messages

--- src/main/java/org/casadocodigo/store/StoreApplication.java
+++ src/main/java/org/casadocodigo/store/StoreApplication.java
@@ -4,10 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableSpringDataWebSupport
@@ -15,16 +12,5 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 public class StoreApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(StoreApplication.class, args);
-	}
-
-	@Bean
-	public ResourceBundleMessageSource messageSource() {
-		ResourceBundleMessageSource source = new ResourceBundleMessageSource();
-
-		source.setBasename("messages");
-		source.setCacheSeconds(3600);
-		source.setDefaultEncoding("UTF-8");
-
-		return source;
 	}
 }
