cbd6287ebefbba721f1ac28713071c10d8574431
Pedro Queiroz <pedro.queiroz@thoughtworks.com>
2019-07-21 19:49:04 -0300

    Improve Home Page

--- src/main/java/org/casadocodigo/store/controller/HomeController.java
+++ src/main/java/org/casadocodigo/store/controller/HomeController.java
@@ -1,13 +1,22 @@
 package org.casadocodigo.store.controller;
 
+import org.casadocodigo.store.dao.ProductDAO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
 
+    @Autowired
+    private ProductDAO productDao;
+
     @RequestMapping("/")
-    public String serveHome() {
-        return "home";
+    public ModelAndView serveHome() {
+        ModelAndView modelAndView = new ModelAndView("home");
+        modelAndView.addObject("products", productDao.list());
+
+        return modelAndView;
     }
 }
